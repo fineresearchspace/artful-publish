f9002c6f2609469bad679f27ec78561ba3f2cb5f
fineresearchspace <srushtisakhare04@gmail.com>
2026-09-25 18:44:43 +0530

    Update articles.$slug.tsx

--- src/routes/articles.$slug.tsx
+++ src/routes/articles.$slug.tsx
@@ -111,6 +111,7 @@ function ArticlePage() {
           </div>
         </header>
 
+        
         <div className="mx-auto max-w-3xl px-4 sm:px-6">
 
           <Markdown content={article.content} className="mt-10" />
